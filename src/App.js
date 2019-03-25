94ace3ad50529145807ad219383288486877035a
William Dale <wmd2011@gmail.com>
2019-03-25 14:25:50 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import PokemonDetails from "./components/PokemonDetails";
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import './App.css';
+import PokemonDetails from './components/PokemonDetails';
+import PokemonTeam from './components/PokemonTeam';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PokemonDetails />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={PokemonDetails} />
+            <Route path="/team" exact component={PokemonTeam} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
